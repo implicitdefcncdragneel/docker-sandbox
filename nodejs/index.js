e540d764663877b661fe43aca3d169beee3658de
Chandra Nandan <chandranandan.chandrakar@gmail.com>
2021-07-25 21:02:12 +0530

    tweaks

--- nodejs/index.js
+++ nodejs/index.js
@@ -3,11 +3,20 @@ const app = express();
 
 
 app.get('/',(req, res) => {
-    res.send('Hello World!');
+    res.json({message:'Hello World!'});
 });
 
+app.get('/login',(req, res) => {
+    res.json({message:'login route'});
+});
 
+app.get('/signup',(req, res) => {
+    res.json({message:'signup route'});
+});
 
+app.get('/logout',(req, res) => {
+    res.json({message:'logout route'});
+});
 
 app.listen(
     8000,() => {console.log('Server is running on port 8000');}
