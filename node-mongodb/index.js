9fc5bb9e4d6fe5fda7083b0ddc590a2aca775b7b
Chandra Nandan <chandranandan.chandrakar@gmail.com>
2021-07-29 23:37:16 +0530

    backendRoute

--- node-mongodb/index.js
+++ node-mongodb/index.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
+const express = require('express');
 
+const app = express();
+
+
+// Database Connection
 const DATABASE ="mongodb://mymongo:27017/example";
 
 mongoose
@@ -14,3 +19,15 @@ mongoose
 .catch(()=>{
     console.log("Error in DB Connection");
 });
+
+
+// Backend
+app.listen(8000,()=>{
+    console.log("Server Port")
+})
+
+app.get("/",(req, res)=>{
+    res.json({
+        message:"root endpoint"
+    })
+})
